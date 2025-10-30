2dcd4b3d7a54536f4fb5e49f9245653a644fdcb4
Sarah Grandke <b383736@levante1.lvt.dkrz.de>
2025-10-30 14:53:01 +0100

    Generalized user

--- software_levante/convert_era5/convert_era5/check_status.sh
+++ software_levante/convert_era5/convert_era5/check_status.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-if [ -z $(squeue -u b382762 -o %.22j | grep $1) ]; then echo 'Finished'; else echo 'running'; fi
+if [ -z $(squeue -u $USER -o %.22j | grep $1) ]; then echo 'Finished'; else echo 'running'; fi
 
